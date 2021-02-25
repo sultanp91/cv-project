7503ba7b3411d820d2fe3f050162f95dcfe5f2f6
Sultan <sultanp@gmail.com>
2021-02-25 02:52:26 +0000

    updated styling in general info component

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,32 +1,25 @@
 import React, { Component } from 'react';
-import '../styles/Generalinfo.css'
+import '../styles/Generalinfo.css';
+import { IoIosSave } from "react-icons/io";
+import { FaUserEdit } from "react-icons/fa";
+
 
 class GeneralInfo extends Component {
     constructor(){
         super()
         this.state = {
             name: "Lorem Ipsum",
+            jobTitle: "Web Developer",
             phoneNumber: "07900000000",
             email: "example@lorem.com",
             form: true
         }
     }
 
-   nameHandler = (evt) => {
+   inputHandler = (evt) => {
+        const key = evt.target.dataset.key
         this.setState({
-            name: evt.target.value
-        })
-    }
-
-    emailHandler = (evt) => {
-        this.setState({
-            email: evt.target.value
-        })
-    }
-
-    numberHandler = (evt) => {
-        this.setState({
-            phoneNumber: evt.target.value
+            [key]: evt.target.value
         })
     }
 
@@ -39,27 +32,44 @@ class GeneralInfo extends Component {
     render() {
         if(this.state.form){
             return (
-                <div>
+                <div className="general-info" >
                     <form className="general-info-form">
-                        <label htmlFor="nameInput">Name: </label>
-                        <input type="text" onChange={this.nameHandler} placeholder="Enter name"
-                         value={this.state.name} id="nameInput"/>
-                         <input type="email" onChange={this.emailHandler} value={this.state.email} 
-                         placeholder="Enter e-mail" id="emailInput"/>
-                         <input type="tel" onChange={this.numberHandler} value={this.state.phoneNumber} 
-                         placeholder="Enter Phone Number" id="phoneInput"/>
+        
+                            <label htmlFor="nameInput">Name: </label>
+                            <input type="text" data-key="name" onChange={this.inputHandler} placeholder="Enter name"
+                            value={this.state.name} id="nameInput"/>
+                            <label htmlFor="jobTitle">Job Title: </label>
+                            <input type="text" data-key="jobTitle" onChange={this.inputHandler} placeholder="Enter job title"
+                            value={this.state.jobTitle} id="jobTitle"/>
+                            <label htmlFor="emailInput">Email: </label>
+                            <input type="email" data-key="email" onChange={this.inputHandler} value={this.state.email} 
+                            placeholder="Enter e-mail" id="emailInput"/>
+                            <label htmlFor="phoneInput">Phone Number: </label>
+                            <input type="tel" data-key="phoneNumber" onChange={this.inputHandler} value={this.state.phoneNumber} 
+                            placeholder="Enter Phone Number" id="phoneInput"/>
+
+
                     </form>
-                    <button onClick={this.editHandler}>Submit</button>
+                    <IoIosSave onClick={this.editHandler} className="general-info-icon" />      
                 </div>
             )
         } else if(!this.state.form){
             return (
                 <div className="general-info">
-                    <h1>Name: {this.state.name}</h1>
-                    <p>Email: {this.state.email}</p>
-                    <p>Number: {this.state.phoneNumber}</p>
-                    <button onClick={this.editHandler}>Edit</button>
+                    <div className="general-info-content">
+                        <h1>{this.state.name}</h1>
+                        <h2>{this.state.jobTitle}</h2>
+                        <hr/>
+                        <div className="contact">
+                        <p>Email: {this.state.email}</p>
+                        <p>Number: {this.state.phoneNumber}</p>
+                        </div>
+                        
+                        <FaUserEdit onClick={this.editHandler} className="general-info-icon" />
+                    </div>
+                    <hr/>
                 </div>
+              
             )
         }
         
